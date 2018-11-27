96b201dcce2f2f3b65049774ae0ecb28451b70f6
DiegoAlbaRuiz-DAM <44582365+DiegoAlbaRuiz-DAM@users.noreply.github.com>
2018-11-28 00:00:21 +0100

    Creacion de Clase Cliente y su Gestor

---
+++ ERP/ERP/Dominio/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Dominio.Gestores
+{
+    class Customer
+    {
+        private int idCustomer { get; set; }
+        private String name { get; set; }
+        private String surname { get; set; }
+        private String address { get; set; }
+        private int phone { get; set; }
+        private String email { get; set; }
+        private int deleted { get; set; }
+        private int refzipcodescities { get; set; }
+
+        private GestorCliente gestorCliente { get; set; }
+
+        public Customer(int idC, String nameC, String surnameC, String addressC, int phoneC, String emailC, int deletedC, int refzipcodescitiesC)
+        {
+            idCustomer = idC;
+            name = nameC;
+            surname = surnameC;
+            address = addressC;
+            phone = phoneC;
+            email = emailC;
+            deleted = deletedC;
+            refzipcodescities = refzipcodescitiesC;
+
+            gestorCliente = new GestorCliente();
+        }
+
+
+
+    }
+}
