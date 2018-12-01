22a61236dd46130f581558b6abef5cb053f46468
EnriqueRubio <enriquerubio97@gmail.com>
2018-12-01 12:16:01 +0100

    Prueba
    prueba

---
+++ ERP/ERP/Presentacion/Categories/AddCategoria.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ERP.Presentacion.Categories
+{
+    public partial class AddCategoria : Form
+    {
+        public AddCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void cargarDiseño() {
+            btnCancelar.BackColor = Color.FromArgb(114, 47, 55);
+            btnCancelar.ForeColor = Color.White;
+            btnCancelar.FlatStyle = FlatStyle.Flat;
+            btnCancelar.FlatAppearance.BorderColor = Color.Black;
+            btnCancelar.FlatAppearance.BorderSize = 1;
+
+            btnSave.BackColor = Color.FromArgb(114, 47, 55);
+            btnSave.ForeColor = Color.White;
+            btnSave.FlatStyle = FlatStyle.Flat;
+            btnSave.FlatAppearance.BorderColor = Color.Black;
+            btnSave.FlatAppearance.BorderSize = 1;
+
+            btnAceptar.BackColor = Color.FromArgb(114, 47, 55);
+            btnAceptar.ForeColor = Color.White;
+            btnAceptar.FlatStyle = FlatStyle.Flat;
+            btnAceptar.FlatAppearance.BorderColor = Color.Black;
+            btnAceptar.FlatAppearance.BorderSize = 1;
+        }
+    }
+}
