b4c0bda1cf8041db40251624ef627a197dcbb148
CICLOS\b14-17m <diego303.d@gmail.com>
2019-01-22 10:35:11 +0100

    Cambios Añadidos
    Añadido proyecto a parte para MultiInserts(por ahora solo incomes)

---
+++ 2-Trimestre/ProgramaMultiInserts/DI-MultiInsert/DI-MultiInsert/Presentacion/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DI_MultiInsert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(488, 214);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(137, 77);
+            this.button2.MaximumSize = new System.Drawing.Size(288, 28);
+            this.button2.MinimumSize = new System.Drawing.Size(288, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(288, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Insert Incomes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(137, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(288, 31);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Insert Expenses";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(575, 249);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnClose);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form MultiInsert";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 2-Trimestre/ProgramaMultiInserts/DI-MultiInsert/DI-MultiInsert/Presentacion/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DI_MultiInsert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConnectOracle conector = new ConnectOracle();
+            decimal cantIncomes = (decimal)conector.DLookUp("COUNT(id)", "incomes_expenses", "");
+            decimal idIncome;
+            if (cantIncomes == 0)
+            {
+                idIncome = 1;
+
+            }
+            else
+            {
+                idIncome = (decimal)conector.DLookUp("MAX(id)", "incomes_expenses", "");
+                idIncome++;
+            }
+
+            Random random = new Random();
+            for (; idIncome < 100; idIncome++)
+            {
+
+                int randomSource = random.Next(0, 3);
+                int randomType = random.Next(0, 3);
+                int randomUser = random.Next(1, 3);
+                newIncome(0, DateTime.Today, (decimal)randomUser, randomSource, randomType, "multiple insert " + idIncome, decimal.Parse("100"));
+            }
+        }
+
+        public void newIncome(decimal id, DateTime income_date, decimal refUser, decimal refSt, decimal refType, string description, decimal amount)
+        {
+            ConnectOracle conector = new ConnectOracle();
+            decimal cantIncomes = (decimal)conector.DLookUp("COUNT(id)", "incomes_expenses", "");
+            decimal idIncome;
+            if (cantIncomes == 0)
+            {
+                idIncome = 1;
+
+            }
+            else
+            {
+                idIncome = (decimal)conector.DLookUp("MAX(id)", "incomes_expenses", "");
+                idIncome++;
+            }
+            MessageBox.Show("INSERT INTO incomes_expenses VALUES ('" + idIncome + "', '" + income_date.ToString().Substring(0, 10)
+                + "','" + refUser + "','" + refSt + "', '" + refType + "', '"
+                + description.Replace('\'', ' ').PadRight(60, ' ').Substring(0, 60).Trim() + "', '"
+                + amount + "', '0','0')");
+            conector.setData("INSERT INTO incomes_expenses VALUES ('" + idIncome + "', '" + income_date.ToString().Substring(0, 10)
+                + "','" + refUser + "','" + refSt + "', '" + refType + "', '"
+                + description.Replace('\'', ' ').PadRight(60, ' ').Substring(0, 60).Trim() + "', '"
+                + amount + "', '0','0')");
+        }
+    }
+}
