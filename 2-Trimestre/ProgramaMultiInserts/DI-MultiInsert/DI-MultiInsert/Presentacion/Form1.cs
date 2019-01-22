59ca6206e8bf7d1181675cc981bf974a09022ff8
CICLOS\b14-17m <diego303.d@gmail.com>
2019-01-22 11:03:29 +0100

    Cambios generales
    Añadida funcion multi expenses en programa multiinserts

--- 2-Trimestre/ProgramaMultiInserts/DI-MultiInsert/DI-MultiInsert/Presentacion/Form1.cs
+++ 2-Trimestre/ProgramaMultiInserts/DI-MultiInsert/DI-MultiInsert/Presentacion/Form1.cs
@@ -45,7 +45,7 @@ namespace DI_MultiInsert
                 int randomSource = random.Next(0, 3);
                 int randomType = random.Next(0, 3);
                 int randomUser = random.Next(1, 3);
-                newIncome(0, DateTime.Today, (decimal)randomUser, randomSource, randomType, "multiple insert " + idIncome, decimal.Parse("100"));
+                newIncome(0, DateTime.Today, (decimal)randomUser, randomSource, randomType, "multiple insert income " + idIncome, decimal.Parse("100"));
             }
         }
 
@@ -73,5 +73,53 @@ namespace DI_MultiInsert
                 + description.Replace('\'', ' ').PadRight(60, ' ').Substring(0, 60).Trim() + "', '"
                 + amount + "', '0','0')");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ConnectOracle conector = new ConnectOracle();
+            decimal cantIncomes = (decimal)conector.DLookUp("COUNT(id)", "incomes_expenses", "");
+            decimal idIncome;
+            if (cantIncomes == 0)
+            {
+                idIncome = 1;
+
+            }
+            else
+            {
+                idIncome = (decimal)conector.DLookUp("MAX(id)", "incomes_expenses", "");
+                idIncome++;
+            }
+
+            Random random = new Random();
+            for (; idIncome < 100; idIncome++)
+            {
+
+                int randomTarget = random.Next(1000, 1004);
+                int randomType = random.Next(0, 3);
+                int randomUser = random.Next(1, 3);
+                newExpense(0, DateTime.Today, (decimal)randomUser, randomTarget, randomType, "multiple insert expense " + idIncome, decimal.Parse("100"));
+            }
+        }
+            public void newExpense(decimal id, DateTime expense_date, decimal refUser, decimal refSt, decimal refType, string description, decimal amount)
+            {
+            ConnectOracle conector = new ConnectOracle();
+            decimal cantExpense = (decimal)conector.DLookUp("COUNT(id)", "incomes_expenses", "");
+                decimal idExpense;
+                if (cantExpense == 0)
+                {
+                    idExpense = 1;
+
+                }
+                else
+                {
+                    idExpense = (decimal)conector.DLookUp("MAX(id)", "incomes_expenses", "");
+                    idExpense++;
+                }
+                conector.setData("INSERT INTO incomes_expenses VALUES (" + idExpense + ", '" + expense_date.ToString().Substring(0, 10)
+                    + "','" + refUser + "','" + refSt + "', '" + refType + "', '"
+                    + description.Replace('\'', ' ').PadRight(60, ' ').Substring(0, 60).Trim() + "', '"
+                    + amount + "', 1,0)");
+            }
     }
+    
 }
